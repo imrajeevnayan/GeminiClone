b89008c625152efc0b8b5ab0aa627b0c6243cb9e
imrajeevnayan <immrrobot121@gmail.com>
2025-08-07 00:49:39 +0530

    intiall commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/GeminiClone/', // Corrected base path
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
